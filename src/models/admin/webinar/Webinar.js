6a189000f7c2ec07657a141c16c7883c25d51e43
Pradh123 <vermapradhumn3@gmail.com>
2024-11-05 10:00:19 +0530

    comment done by pradhumn verma

---
+++ src/models/admin/webinar/Webinar.js
@@ -0,0 +1,55 @@
+const { default: mongoose } = require("mongoose");
+
+const webinarSchema=new mongoose.Schema({
+    webinarImage:{
+        type:String,
+        required:true
+    },
+    imgAlt:{
+        type:String
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    videoLink:{
+        type:String,
+        required:true
+    },
+    sublitile:{
+        type:String,
+        required:true
+    },
+    webinarType:{
+        type:String,
+        required:true
+    },
+    toolsAndSoftware:{
+        type:String,
+        required:true
+    },
+    topics:{
+        type:String,
+        required:true
+    },
+    department:{
+        type:String,
+        required:true
+    },
+    industry:{
+        type:String,
+        required:true
+    },
+    languages:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    }
+
+},{timestamps:true});
+
+const Webinar=mongoose.models.Webinar||mongoose.model("Webinar",webinarSchema);
+export default Webinar;
