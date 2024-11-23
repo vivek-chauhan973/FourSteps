3f5a63e7f2e23f108855f0c553525cc30b815994
vivek-chauhan2 <vivekchauhan1181@gmail.com>
2024-11-23 15:52:07 +0530

    just trail

--- src/Component/Web/Calltoaction.jsx
+++ src/Component/Web/Calltoaction.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import Image from "next/image";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Calltoaction = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1200,
+      once: true,
+    });
+  }, []);
   return (
     <div>
-      <div className="relative container mb-10">
+      <div className="relative container mb-10" data-aos="fade-up">
         <div className="absolute inset-0 bg-gradient-to-t mx-4 from-orange-500 to-transparent"></div>
         <div className="relative text-black py-3">
           <div className="container mx-auto ">
             <div className="flex flex-col gap-10 md:flex-row items-center justify-between bg-gray-100 p-8 z-10 relative">
               <div className="md:w-1/3  mb-4 md:mb-0">
-                <img
+                <Image
                   src="/image/ab.webp"
                   alt="Consultant"
                   className="w-full h-full  object-cover "
+                  height={100}
+                  width={100}
                 />
               </div>
               <div className="md:w-2/3 text-center md:text-left">
@@ -54,26 +64,3 @@ const Calltoaction = () => {
 };
 
 export default Calltoaction;
-{
-  /* <div class="max-w-6xl my-10 mx-auto p-6  shadow-md rounded-lg relative overflow-hidden">
-        <div class="absolute inset-y-0 left-0 w-1 bg-orange-500"></div>
-
-        <div class="relative z-10 grid grid-cols-1 md:grid-cols-2 gap-6 p-5 items-center">
-          <div>
-            <h2 class="text-2xl py-6 md:text-3xl font-bold text-gray-800">
-              Onboarding and Unleash the Power of Zoho for{" "}
-              <span class="text-orange-500">Your Business</span>
-            </h2>
-            <p class="py-4 text-sm md:text-base text-gray-600">
-              Transform your business through tailored guidance and training to
-              fully leverage the capabilities of Zoho for growth.
-            </p>
-          </div>
-          <div class="flex justify-center">
-            <button class="mt-6 md:mt-0 w-56 h-14 bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-full shadow-lg transition">
-              CONNECT WITH US
-            </button>
-          </div>
-        </div>
-      </div> */
-}
