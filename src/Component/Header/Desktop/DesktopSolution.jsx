ba6a256ebeaa494a5561408666c673d0f154a528
vivek-chauhaan <vivekchauhan1181@gmail.com>
2025-01-21 16:34:54 +0530

    kmjnhgb

--- src/Component/Header/Desktop/DesktopSolution.jsx
+++ src/Component/Header/Desktop/DesktopSolution.jsx
@@ -1,182 +1,4 @@
-// import React, { useEffect, useState } from "react";
-// import Link from "next/link";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faChevronRight } from "@fortawesome/free-solid-svg-icons"; // Importing FontAwesome icons
-
-// const fetchAllSoutionType = async () => {
-//   const res = await fetch("/api/solution/masterS", { method: "GET" });
-//   return await res.json();
-// };
-// // The services data remains the same static data dont use
-// const solution = [
-//   {
-//     name: "Zoho Based Solution",
-//     content: {
-//       heading: "Zoho Based Solution",
-
-//       links: [
-//         { name: "Zoho Travel Agency CRM", link: "/zoho-travel-agency-crm" },
-//         {
-//           name: "Zoho RealEstate Agency CRM",
-//           link: "/zoho-realestate-agency-crm",
-//         },
-//         {
-//           name: "Zoho RealEstate Developer CRM",
-//           link: "/zoho-realestate-developer-crm",
-//         },
-//         { name: "Zoho Finance Agency CRM", link: "/zoho-finance-agency-crm" },
-//         { name: "Zoho Healthcare CRM", link: "/zoho-healthcare-crm" },
-//       ],
-//     },
-//   },
-//   {
-//     name: "Our Hubspot  Products",
-//     content: {
-//       heading: "Our Custom Products",
-
-//       links: [
-//         {
-//           name: "Tour & Package Management System",
-//           link: "/tour-package-management-system",
-//         },
-//         {
-//           name: "Car Rental Booking System",
-//           link: "/car-rental-booking-system",
-//         },
-//         {
-//           name: "Hotel Booking Management System",
-//           link: "/hotel-booking-management-system",
-//         },
-//         {
-//           name: "Hospital/Clinics Management System",
-//           link: "/hospital-clinics-management-system",
-//         },
-//       ],
-//     },
-//   },
-//   {
-//     name: "Our Custom Products",
-//     content: {
-//       heading: "Our Custom Products",
-
-//       links: [
-//         {
-//           name: "Tour & Package Management System",
-//           link: "/tour-package-management-system",
-//         },
-//         {
-//           name: "Car Rental Booking System",
-//           link: "/car-rental-booking-system",
-//         },
-//         {
-//           name: "Hotel Booking Management System",
-//           link: "/hotel-booking-management-system",
-//         },
-//         {
-//           name: "Hospital/Clinics Management System",
-//           link: "/hospital-clinics-management-system",
-//         },
-//         {
-//           name: "Learning Management System",
-//           link: "/learning-management-system",
-//         },
-//         {
-//           name: "Finance Agency Management System (Upcoming Product)",
-//           link: "/finance-agency-management-system-upcoming",
-//         },
-//       ],
-//     },
-//   },
-// ];
-
-// const DesktopSolution = ({ activeLink, handleLinkClick }) => {
-//   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-//   const [activeService, setActiveService] = useState(solution[0]);
-//   const [solutionType, setSolutionType] = useState([]);
-//   console.log("...................vivek--->", solutionType);
-//   useEffect(() => {
-//     fetchAllSoutionType()
-//       .then((res) => {
-//         console.log("all slution types abc is here ---> ", res);
-//         setSolutionType(res.data);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching solution types:", error);
-//       });
-//   }, []);
-//   // Set default to the first service
-
-//   return (
-//     <li
-//       className="relative inline-block "
-//       onMouseEnter={() => setIsDropdownOpen(true)}
-//       onMouseLeave={() => setIsDropdownOpen(false)}
-//     >
-//       <Link
-//         href="#"
-//         onClick={() => handleLinkClick("/#")}
-//         className={`relative inline-block text-base font-medium px-3 py-2 ${
-//           activeLink === "/#" ? "text-orange-500" : "text-gray-800"
-//         } hover:text-orange-500`}
-//       >
-//         Solution
-//         <span
-//           className={`absolute left-0 bottom-0 h-0.5 w-full bg-orange-500 transition-all duration-300 transform ${
-//             activeLink === "/#" ? "scale-x-100" : "scale-x-0"
-//           }`}
-//         ></span>
-//       </Link>
-
-//       {/* Dropdown Menu */}
-//       {isDropdownOpen && (
-//         <div className="absolute left-[-10vw]  w-[65vw]  bg-background  border  ">
-//           <div className="flex max-w-7xl mx-auto">
-//             {/* Sidebar Menu */}
-//             <div className="w-1/4  border-r py-2 ">
-//               <h2 className="text-lg  border-b  py-2 pl-2 font-semibold">
-//                 Solutions
-//               </h2>
-
-//               {solutionType?.map((solution) => (
-//                 <div
-//                   key={solution._id} // Use _id as a unique key
-//                   className="py-3 text-md rounded font-normal px-4 cursor-pointer bg-white hover:bg-background flex justify-between transition-colors"
-//                   onMouseEnter={() => setActiveService(solution)}
-//                   onClick={() => setActiveService(solution)}
-//                 >
-//                   <span>{solution?.name}</span>
-//                   <FontAwesomeIcon icon={faChevronRight} className="text-sm" />
-//                 </div>
-//               ))}
-//             </div>
-//             {/* Content Area */}
-
-//             <div className="flex-1 pl-10 py-4">
-//               <h2 className="text-xl font-semibold  border-b  transition duration-200">
-//                 {activeService.content.heading}
-
-//               </h2>
-
-//               {/* <div className=" space-y-2">
-//                 {activeService.content.links.map((link) => (
-//                   <Link key={link.name} href={link.link}>
-//                     <p className=" text-gray-800 text-sm py-1  hover:text-orange-500 transition duration-200">
-//                       {link.name}
-//                     </p>
-//                   </Link>
-//                 ))}
-//               </div> */}
-//             </div>
-//           </div>
-//         </div>
-//       )}
-//     </li>
-//   );
-// };
-
-// export default DesktopSolution;
-
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons"; // Importing FontAwesome icons
@@ -190,6 +12,14 @@ const DesktopSolution = ({ activeLink, handleLinkClick }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [activeService, setActiveService] = useState(null);
   const [solutionType, setSolutionType] = useState([]);
+  const [solutionId,setSolutionId]=useState(null);
+
+const memoizedCallback = useCallback(
+  () => {
+    console.log("solution id is here is as------> ",solutionId)
+  },
+  [solutionId], 
+);
 
   useEffect(() => {
     fetchAllSoutionType()
@@ -240,7 +70,7 @@ const DesktopSolution = ({ activeLink, handleLinkClick }) => {
                 <div
                   key={solution?._id}
                   className="py-3 text-md rounded font-normal px-4 cursor-pointer bg-white hover:bg-background flex justify-between transition-colors"
-                  onMouseEnter={() => setActiveService(solution)}
+                  onMouseEnter={() => {setActiveService(solution);setSolutionId(solution?._id)}}
                   onClick={() => setActiveService(solution)}
                 >
                   <span>{solution?.name}</span>
