919615ac71279373911198ffc596551c17a8ab6d
vivek-chauhaan <vivekchauhan1181@gmail.com>
2025-02-20 17:50:03 +0530

    some cnage

--- src/pages/api/admin/protected-route.js
+++ src/pages/api/admin/protected-route.js
@@ -1,4 +1,12 @@
-import { verifyAdmin } from "@/middlewares/verifyAdmin";
-export default verifyAdmin(async function handler(req, res) {
-  res.json({ message: 'Admin API accessed successfully' });
-});
+// import { verifyAdmin } from "@/middlewares/verifyAdmin";
+// export default verifyAdmin(async function handler(req, res) {
+//   res.json({ message: 'Admin API accessed successfully' });
+// });
+
+// src/middlewares/verifyAdmin.js
+export const verifyAdmin = (handler) => async (req, res) => {
+  if (!req.user || req.user.role !== "admin") {
+    return res.status(403).json({ message: "Access denied" });
+  }
+  return handler(req, res);
+};
